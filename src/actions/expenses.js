266c2b02d8bcdd3bc24448784b4dd9bbb46c0a19
Jeff Lucas <jslucas1@msn.com>
2020-07-11 16:08:27 -0500

    added startEditExpense functionality

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -42,7 +42,15 @@ export const editExpense = (id, updates) => ({
     type: 'EDIT_EXPENSE',
     id,
     updates
-})
+});
+
+export const startEditExpense = (id, updates) => {
+    return (dispatch) => {
+        return database.ref(`expenses/${id}`).update(updates).then(() => {
+            dispatch(editExpense(id, updates));
+        });
+    }
+};
 
 export const setExpenses = (expenses) => ({
     type: 'SET_EXPENSES',
