ca818301fab1259c92810d0e0c71781a6e54d3dc
Jeff Lucas <jslucas1@msn.com>
2020-07-12 08:20:22 -0500

    added google auth login

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -29,22 +29,6 @@ test('should setup remove expense action option', () => {
     });
 });
 
-// test('should remove expense from firebase', (done) => {
-//     const store = createMockStore({});
-//     const id = expenses[2].id;
-//     store.dispatch(startRemoveExpense({id})).then(()=>{
-//         const actions = store.getActions();
-//         expect(actions[0].toEqual({
-//             type: 'REMOVE_EXPENSE',
-//             id
-//         }));
-//         return database.ref(`expenses/${id}`).once('value');
-//     }).then((snapshot) => {
-//         expect(snapshot.val()).toBeFalsy();
-//         done();
-//     });
-// });
-
 test('should remove expense from firebase', (done) => {
     const store = createMockStore({});
     const id = expenses[2].id;
