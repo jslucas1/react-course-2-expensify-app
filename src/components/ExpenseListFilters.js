69ecb64bf9d2d8cd3ca06d06087632be04a82e59
Jeff Lucas <jslucas1@msn.com>
2020-07-13 08:42:34 -0500

    finished styling and added babel polyfill

--- src/components/ExpenseListFilters.js
+++ src/components/ExpenseListFilters.js
@@ -29,13 +29,16 @@ export class ExpenseListFilters extends React.Component {
                 <div className="input-group">
                     <div className="input-group__item">
                         <input 
-                            type="text" 
+                            type="text"
+                            className = "text-input" 
+                            placeholder="Search expenses"
                             value={this.props.filters.value} 
                             onChange={this.onTextChange}
                         />
                     </div>
                     <div className="input-group__item">
                         <select 
+                            className = "select"
                             value={this.props.filters.value} 
                             onChange={this.onSortChange}
                         >
